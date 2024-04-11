6d668a21249bb9502386c0479dfc3086feea9c09
masungil <masugil2@daum.net>
2024-04-11 16:52:23 +0900

    인증을 위해 UserDetailsService, UserDetails을 직접 구현 하여 설정함

--- src/main/java/org/kosa/hello/entity/MemberVO.java
+++ src/main/java/org/kosa/hello/entity/MemberVO.java
@@ -1,5 +1,11 @@
 package org.kosa.hello.entity;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +15,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class MemberVO {
+public class MemberVO implements UserDetails {
 	
 	private String member_id;
 	private String member_pwd;
@@ -20,5 +26,42 @@ public class MemberVO {
 	
 	public boolean isEqualsPwd(String pwd) {
 		return this.member_pwd.equals(pwd);		
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Collection<GrantedAuthority> collections = new ArrayList<GrantedAuthority>();
+		collections.add(() -> "ROLE_USER");
+		return collections;
+	}
+
+	@Override
+	public String getPassword() {
+		return member_pwd;
+	}
+
+	@Override
+	public String getUsername() {
+		return member_id;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
--- src/main/java/org/kosa/hello/member/MemberService.java
+++ src/main/java/org/kosa/hello/member/MemberService.java
@@ -2,6 +2,10 @@ package org.kosa.hello.member;
 
 import org.kosa.hello.entity.BoardVO;
 import org.kosa.hello.entity.MemberVO;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -16,19 +20,31 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
       
 	private final MemberMapper  memberMapper;
 
-	public MemberVO login(MemberVO memberVO)  {
-		//view Count의 값이 증가된 객체를 얻는다
-		MemberVO resultVO = memberMapper.login(memberVO);
-		if (resultVO != null && memberVO.isEqualsPwd(resultVO.getMember_pwd())) {
-			return resultVO;
-		}
+//	public MemberVO login(MemberVO memberVO)  {
+//		//view Count의 값이 증가된 객체를 얻는다
+//		MemberVO resultVO = memberMapper.login(memberVO);
+//		if (resultVO != null && memberVO.isEqualsPwd(resultVO.getMember_pwd())) {
+//			return resultVO;
+//		}
+//		return null;
+//	}
+
+	public static void main(String [] args) {
+		BCryptPasswordEncoder bcryptPasswordEncoder = new BCryptPasswordEncoder();
+		System.out.println(bcryptPasswordEncoder.encode("1004"));
+		System.out.println(bcryptPasswordEncoder.encode("0123456789010234567890123456789"));
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		MemberVO resultVO = memberMapper.login(MemberVO.builder().member_id(username).build());
+		
 		return null;
 	}
-	
 }
 
 
