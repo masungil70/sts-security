f91929920bba15f962bd3d23799adb0b10a95a8d
masungil <masugil2@daum.net>
2024-04-11 12:51:56 +0900

    기본 스프링 시큐리티 적용한 코드

---
+++ src/main/java/org/kosa/hello/BoardController.java
@@ -0,0 +1,15 @@
+package org.kosa.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@GetMapping("/list")
+	public void list() {
+		System.out.println("board.list() 함수 호출");
+	}
+}
