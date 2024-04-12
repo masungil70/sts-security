4e67d54db111bf7b5c3b4cf441d811c61c7bb846
masungil <masungil70.gmail.com>
2024-04-12 23:18:11 +0900

    어노테이션을 사용하여 인증객체를 접근 하는 예제

--- src/main/java/org/kosa/hello/BoardController.java
+++ src/main/java/org/kosa/hello/BoardController.java
@@ -6,7 +6,7 @@ import org.kosa.hello.entity.MemberVO;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
